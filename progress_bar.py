5489fcb1e1bc8e694fcaca2bba501027773fb835
Artem Kulikov <chigar2010@yandex.ru>
2024-05-05 22:55:43 +0200

    add

---
+++ progress_bar.py
@@ -0,0 +1,10 @@
+from time import sleep
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    # Передача в функцию tqdm() итерируемого объекта
+    for i in tqdm(range(2000), desc='Мой первый прогресс-бар'):
+        sleep(0.003)
+    print('Ну наконец-то это случилось! ДОЖДАЛИСЬ!')
+    for i in tqdm(range(1000), desc='А вот и второй'):
+        sleep(0.001)
